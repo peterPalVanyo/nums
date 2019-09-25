a1a62da7361ed298d47369bab9e2fe4145b58d43
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-09-25 15:12:47 +0200

    info button

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-const btn = document.querySelector('.btn')
+const filter = document.querySelector('#btns .btn:first-child')
+const info = document.querySelector('#btns .btn:nth-child(2)')
+
 const img = document.querySelector('#img')
 const icons = document.querySelectorAll('.icon')
 const allImg = ['./picture/0101.svg', './picture/0102.svg', './picture/0103.svg', './picture/0104.svg', './picture/0105.svg', './picture/0106.svg']
+const titles = ['KEDVENC', 'TIPP', 'VONZALOM', 'NEHÉZ', 'JÓ', 'IZGI']
+const title = document.querySelector('#box h2')
+const text = document.querySelector('#text')
 
 
-btn.addEventListener('click', function() {
+filter.addEventListener('click', function() {
     const path = img.getAttribute('src')
     const num = img.getAttribute('src').slice(13, 14)
-    console.log(`${path.slice(0,13)}2.svg`)
     num === '1' ? img.setAttribute('src', `${path.slice(0,13)}2.svg`) : img.setAttribute('src', `${path.slice(0,13)}1.svg`)
 })
 
@@ -16,5 +20,12 @@ icons.forEach(function(icon) {
         const num = icon.getAttribute('id').slice(1)
         const path = `./picture/0${num}01.svg`
         img.setAttribute('src', path)
+
+        title.textContent = titles[num-1]
     })
+})
+
+info.addEventListener('click', function() {
+    text.style.visibility === 'hidden' ? text.style.visibility = 'visible' : text.style.visibility = 'hidden'
+    console.log(text.getAttribute('visibility') === 'hidden')
 })
